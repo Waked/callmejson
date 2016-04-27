122c8dfbdc76a6f137e50288b4c74a06e2bce918
Waked <komma336@gmail.com>
2016-04-27 16:29:02 +0200

    Cmon it's not that hard

--- include/json_in.h
+++ include/json_in.h
@@ -1,13 +1,7 @@
 #ifndef JSON_IN_H
 #define JSON_IN_H
 
-std::string readWord(std::ifstream&);
-std::string readKey(std::ifstream&);
-void toNextValue(std::ifstream&);
-ElementType checkTypeOf(char);
-bool isAtObjectEnd(std::ifstream&);
-Element* readValueFrom(std::ifstream&);
-Element* readKVPair(std::ifstream&, bool = 0);
-void loadJSON(std::ifstream&, Object&);
+Object* readSquares(ifstream& in);
+Object* readBracket(ifstream& in);
 
 #endif // JSON_IN_H
--- src/json_in.cpp
+++ src/json_in.cpp
@@ -1,4 +1,6 @@
 #include <fstream>
+#include <sstream>
+#include <iostream>
 #include "../include/Element.hpp"
 #include "../include/Object.hpp"
 #include "../include/Property.hpp"
@@ -6,111 +8,53 @@
 
 using namespace std;
 
-/// ------------------------ ONCE AGAIN --------------------------
+/// ------------------------ ONCE FUCKING AGAIN --------------------------
 
-///Reads characters up to next parenthesis and stores
-///them in a string variable, then returns the string
-string readWord(ifstream& instream){
-    string tempstr = "";
-    char tempc;
-    instream >> tempc;
-    while (tempc != '"'){
-        tempstr += tempc;
-        instream >> tempc;
-    }
-    return tempstr;
+Object* readSquares(ifstream& inputstr){
+
 }
 
-///Returns a string containing the next parenthesed
-///word in stream
-string readKey(ifstream& instream){
-    char tempchar = 0;
-    string tempstr = "";
-    while (tempchar != '"')
-        instream >> tempchar;
-    return readWord(instream);
-}
-
-///Reads through characters (mainly whitespaces and
-///doubledots) up to next character indicating a value
-void toNextValue(ifstream& instream){
-    char tmpchar = '\0';
-    while (tmpchar != '"' && tmpchar != '{' && tmpchar != '[')
-        instream >> tmpchar;
-    instream.putback(tmpchar);
-}
-
-///Returns the type of value that starts with
-///given character
-ElementType checkTypeOf(char ch){
-    if (ch == '"')
-        return PROPERTY;
-    if (ch == '{')
-        return OBJECT;
-    if (ch == '[')
-        return ARRAY;
-}
-
-///Reads the stream through whitespaces and returns
-///whether it's the object-closing character or not
-bool isAtObjectEnd(ifstream& instream){
-    char tmpchar = ' ';
-    while (tmpchar == ' ')
-        instream >> tmpchar;
-    if (tmpchar == '}' || tmpchar == ']')
-        return true;
-    else {
-        instream.putback(tmpchar);
-        return false;
-    }
-}
-
-///Checks next value-opening character and allocates
-///appropriate object in memory, populates the object
-///with data, then returns a pointer to the object
-Element* readValueFrom(ifstream& instream){
-    char openingchar;
-    ElementType valuetype;
-    Element* kvpair;
-    toNextValue(instream);
-    instream >> openingchar;
-    valuetype = checkTypeOf(openingchar);
-    if (valuetype == PROPERTY){
-        Property* returnobject = new Property;
-        returnobject->setElemType(valuetype);
-        returnobject->setValue(readWord(instream));
-        return returnobject;
-    } else {
-        Object* returnobject = new Object;
-        returnobject->setElemType(valuetype);
-        while (!isAtObjectEnd(instream)){
-            kvpair = readKVPair(instream, (valuetype == ARRAY));
-            returnobject->addElement(kvpair);
-            (*returnobject)++;
-        }
-        return returnobject;
-    }
-}
-
-///Returns an address to allocated Element with key and value
-Element* readKVPair(ifstream& instream, bool without_key){
-    Element* outputelement;
-    string key = "";
-    if (!without_key)
-        key = readKey(instream);
-    outputelement = readValueFrom(instream);
-    outputelement->setKey(key);
-    return outputelement;
-}
-
-///Loads JSON document from file input stream into
-///target root object of type Object, returns true
-///on succes and false on failure
-void loadJSON(ifstream& instream, Object& target){
+/// DONE
+Object* readBracket(ifstream& inputstr){
+    Object* returnobj = new Object;
+    Element* pushelem;
+    stringbuf tempstr;
     char tempchar;
-    Element* mainkvpair;
-    instream >> tempchar;
-    mainkvpair = readKVPair(instream, 0);
-    target = *(dynamic_cast<Object*>(mainkvpair));
-    delete mainkvpair;
+
+    inputstr >> tempchar;
+    if (tempchar != '{'){
+        cout << "Trying to read nonbracket!" << endl;
+        return nullptr;
+    }
+
+    inputstr >> tempchar;
+    while (tempchar != '}'){
+        inputstr.get(tempstr, '"');
+        inputstr >> tempchar; ///kropka
+        inputstr >> tempchar;
+
+        switch (tempchar){
+        case '"':
+            pushelem = new Property;
+            pushelem->setKey(tempstr.str());
+            inputstr.get(tempstr, '"');
+            pushelem->setValue(tempstr.str());
+            break;
+        case '{':
+            inputstr.putback(tempchar);
+            pushelem = readBracket(inputstr);
+            pushelem.setKey(tempstr.str());
+            break;
+        case '[':
+            inputstr.putback(tempchar);
+            pushelem = readSquares(inputstr);
+            pushelem.setKey("");
+            break;
+        }
+        inputstr >> tempchar;
+        if (tempchar == ',')    /// przecinek
+            inputstr >> tempchar;
+    }
+
+    return returnobj;
 }
